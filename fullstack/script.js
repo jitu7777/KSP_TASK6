2a6b8d215a201d8545deb260b7e886daf1ac6b49
jitu7777 <150154530+jitu7777@users.noreply.github.com>
2023-11-13 13:15:20 +0530

    Add files via upload

---
+++ fullstack/script.js
@@ -0,0 +1,93 @@
+window.alert("this is jitu");
+
+console.log("Hello");
+
+//Variable assign
+number1 = 4;
+
+console.log(number1);
+
+//let assign
+let number = 20;
+
+console.log(number);
+
+//CamelCase
+thisIsTaskNumber = "task number 5";
+
+console.log(thisIsTaskNumber);
+
+//Constant
+const name = "jithendra";
+
+console.log(name);
+
+/*
+
+
+this 
+is 
+multi-line
+comment
+
+*/
+
+// Operators
+let num1 = 2;
+let num2 = 4;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+// assignment operators
+num1+=10
+console.log(num1)
+num1 -=1
+console.log(num1)
+num1 *=2
+console.log(num1)
+num1**=2
+console.log(num1)
+// comparision operators
+let l1 = 10;
+let name = "10"
+let l2 = 15;
+console.log(n1==name);
+console.log(n1===name);
+console.log(n1!=name);
+console.log(n1<=n2);
+console.log(n1<n2);
+console.log(n1>=n2);
+// logical operators
+let x = true;
+let y = false;
+console.log(!x);
+console.log(x && y);
+console.log(x || y);
+// functions
+function jitu(){
+    console.log("welcome to jitus world");
+}
+jitu()
+let n1 = parseInt(prompt("Enter num1:"));
+let n2 = parseInt(prompt("Enter num2:"));
+function Addition(){
+    console.log(n1+n2);
+}   
+
+function Subtraction(){
+    console.log(n1-n2);
+}   
+
+function Multiplication(){
+    console.log(n1*n2);
+}  
+function Division(){
+    console.log(n1/n2);
+}
+Addition(n1,n2);
+Subtraction(n1,n2);
+Multiplication(n1,n2);
+Division(n1,n2);
